401cf1fd7308a85ac31dcb91f315b1306df812df
Alexander Lopez <alc220196@hotmail.com>
2020-04-12 01:05:28 -0500

    Initial Commit

---
+++ AllNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllNews
+//
+//  Created by Alexander Lopez Cedillo on 12/04/20.
+//  Copyright © 2020 Alexander Lopez Cedillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
